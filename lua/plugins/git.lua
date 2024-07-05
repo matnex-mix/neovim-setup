4784c9c344fe07c8552e8a79fae6d4455db3c3c3
Matnex <jolaoshobatmat@gmail.com>
2024-07-05 14:57:35 +0100

    rust, trouble, git, floaterm

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -8,4 +8,12 @@ return {
       require('gitsigns').setup()
     end
   },
+  {
+    'voldikss/vim-floaterm',
+    config = function ()
+      --vim.keymap.set('n', "<leader>ft", ':FloatermNew --height=0.6 --width=0.4 --wintype=float --name=floaterm1 --position=topleft --autoclose=2 powershell -NoExit -Command "cd ./"<CR>', {})
+      vim.keymap.set('n', "t", ":FloatermToggle myfloat<CR>", {})
+      vim.keymap.set('t', "<Esc>", "<C-\\><C-n>:q<CR>", {})
+    end
+  }
 }
