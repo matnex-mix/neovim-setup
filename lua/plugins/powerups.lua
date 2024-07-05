50f6d3ffd3de7f7decd9b0804af9022facf8dd73
Matnex <jolaoshobatmat@gmail.com>
2024-07-05 16:27:57 +0100

    added todo-comments

--- lua/plugins/powerups.lua
+++ lua/plugins/powerups.lua
@@ -1,20 +1,36 @@
+-- TODO: add more power ups
+-- HACK: hacking the world
+
 return {
   {
-      'smoka7/hop.nvim',
-      version = "*",
-      opts = {
-          keys = 'etovxqpdygfblzhckisuran'
-      }
+    "smoka7/hop.nvim",
+    version = "*",
+    opts = {
+      keys = "etovxqpdygfblzhckisuran",
+    },
   },
   {
     "preservim/tagbar",
     config = function()
-      vim.keymap.set('n', '<F8>', ':TagbarToggle<CR>')
-    end
+      vim.keymap.set("n", "<F8>", ":TagbarToggle<CR>")
+    end,
   },
   {
-  "folke/todo-comments.nvim",
+    "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {}
-  }
+    opts = {},
+    config = function()
+      local todo_comments = require('todo-comments')
+
+      todo_comments.setup()
+
+      vim.keymap.set("n", "]t", function()
+        todo_comments.jump_next()
+      end, { desc = "Next todo comment" })
+
+      vim.keymap.set("n", "[t", function()
+        todo_comments.jump_prev()
+      end, { desc = "Previous todo comment" })
+    end,
+  },
 }
